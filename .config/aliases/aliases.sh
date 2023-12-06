00fc25b69619b971bae8cdbd918ac96cc68e6c0a
Coko <91132775+Coko7@users.noreply.github.com>
2023-12-06 11:22:16 +0100

    rearrange aliases

--- .config/aliases/aliases.sh
+++ .config/aliases/aliases.sh
@@ -17,30 +17,20 @@ alias any1='ping 8.8.8.8'
 
 alias ls='eza --icons -a --group-directories-first'
 alias ll='ls -alF'
-alias gg='git grep -n'
-alias ggu='git grep -n --no-index'
 alias upgrates='sudo apt update && apt list --upgradable && sudo apt upgrade'
-alias ggv='function _f(){ git grep -n $1 | grep -v $2 | grep -n --color=always $1 | less; }; _f'
-alias grasp='function grasp(){ grep -inr "$1" --exclude-dir={obj,bin} --exclude=\*.min.\*; }; grasp'
-#alias nvim='~/nvim-linux64/bin/nvim'
 alias vim='nvim'
 alias fs='ranger'
 alias soz='source $ZDOTDIR/.zshrc'
 
-# Vim aliases
-alias valias='vim $XDG_CONFIG_HOME/aliases/aliases.sh'
-alias vgalias='vim $XDG_CONFIG_HOME/aliases/gitaliases.sh'
-alias vjump='vim $XDG_CONFIG_HOME/aliases/kjump.sh'
-alias vzsh='vim $ZDOTDIR/.zshrc'
-alias vspace='vim $ZDOTDIR/spaceship.zsh'
-alias vmux='vim $XDG_CONFIG_HOME/tmux/tmux.conf'
-
-# Source Marco Fontani's custom git-grepblame command
-# See: https://blog.darkpan.com/posts/git-grepblame/
-source $HOME/scripts/git-grepblame.sh
-
 function weather() {
   curl wttr.in/${1}
 }
+
+# CD quick aliases
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias cd....='cd ../../..'
+alias cd.....='cd ../../../..'
+alias cd......='cd ../../../../..'
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
--- .config/aliases/gitaliases.sh
+++ .config/aliases/gitaliases.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
 alias gst='git status'
 
 alias ga='git add'
 alias gaa='git add --all'
+
+alias gg='git grep -n'
+alias gu='git grep -n --no-index'
+function ggv() { git grep -n $1 | grep -v $2 | grep -n --color=always $1 | less; }
+function grasp() { grep -inr "$1" --exclude-dir={obj,bin} --exclude=\*.min.\*; }
 
 alias gp='git push'
 alias gpo='git push origin'
@@ -32,3 +39,7 @@ alias gpl='git pull'
 alias gplo='git pull origin'
 
 alias gstl='git stash list'
+
+# Source Marco Fontani's custom git-grepblame command
+# See: https://blog.darkpan.com/posts/git-grepblame/
+source $HOME/scripts/git-grepblame.sh
