13525b282ff8362b51b276d90a661837552787fe
Coko <91132775+Coko7@users.noreply.github.com>
2024-01-04 07:43:22 +0100

    edit set-wallpaper to accept optional argument to pick specific image

--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+
+wallp_dir="$HOME/Pictures/Wallpapers"
+
+# Pick random wallpaper if no argument supplied
+if [ "$#" -eq 0 ]; then
+  pick=$(fd . $wallp_dir -t f -e '.jpg' -e '.png' -e '.webp' | shuf -n1)
+else
+  pick=$1
+fi
+
+feh --no-fehbg --bg-fill $pick
+
 #feh --no-fehbg --bg-scale '/home/coco/Pictures/Wallpapers/kame-house.jpg' 
-fd . $HOME/Pictures/Wallpapers -t f -e '.jpg' -e '.png' -e '.webp' -0 | shuf -n1 -z | xargs -0 feh --no-fehbg --bg-fill
+#fd . $wallp_dir -t f -e '.jpg' -e '.png' -e '.webp' -0 | shuf -n1 -z | xargs -0 feh --no-fehbg --bg-fill
