216df0622013b3d11fd6b11e5b5fee0822ec4ad5
Coko <91132775+Coko7@users.noreply.github.com>
2024-01-13 12:26:26 +0100

    scripts: edit set-wallpaper to use feh for picking a random wallpaper

--- .config/scripts/set-wallpaper.sh
+++ .config/scripts/set-wallpaper.sh
@@ -4,12 +4,11 @@ wallp_dir="$HOME/Pictures/Wallpapers"
 
 # Pick random wallpaper if no argument supplied
 if [ "$#" -eq 0 ]; then
-  pick=$(fd . $wallp_dir -t f -e '.jpg' -e '.png' -e '.webp' | shuf -n1)
+  # pick=$(fd . $wallp_dir -t f -e '.jpg' -e '.png' -e '.webp' | shuf -n1)
+  feh --no-fehbg --randomize --bg-fill $wallp_dir/*
 else
-  pick=$1
+  feh --no-fehbg --bg-fill $1
 fi
-
-feh --no-fehbg --bg-fill $pick
 
 #feh --no-fehbg --bg-scale '/home/coco/Pictures/Wallpapers/kame-house.jpg' 
 #fd . $wallp_dir -t f -e '.jpg' -e '.png' -e '.webp' -0 | shuf -n1 -z | xargs -0 feh --no-fehbg --bg-fill
