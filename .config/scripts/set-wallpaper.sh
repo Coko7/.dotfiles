751d78edb3357bc5feb1d000fc73d4033482c8df
Coko <91132775+Coko7@users.noreply.github.com>
2024-07-18 16:23:05 +0200

    scripts: improve set-wallpaper

--- .config/scripts/set-wallpaper.sh
+++ .config/scripts/set-wallpaper.sh
@@ -1,13 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-wallp_dir="$HOME/Pictures/Wallpapers"
+def_wp_dir="$HOME/Pictures/Wallpapers"
 
-# Pick random wallpaper if no argument supplied
+# If no arg supplied, pick random img from default wallpapers dir
 if [ "$#" -eq 0 ]; then
-  # pick=$(fd . $wallp_dir -t f -e '.jpg' -e '.png' -e '.webp' | shuf -n1)
-  feh --no-fehbg --bg-fill --randomize --recursive $wallp_dir
+    # pick=$(fd . $wallp_dir -t f -e '.jpg' -e '.png' -e '.webp' | shuf -n1)
+    feh --no-fehbg --bg-fill --randomize --recursive $def_wp_dir
 else
-  feh --no-fehbg --bg-fill $1
+    # If arg is dir, take random image from it
+    if [ -d "$1" ]; then
+        feh --no-fehbg --bg-fill --randomize --recursive $1
+    # If arg if an image file, set is as wp otherwise exit with failure
+    else
+        declare file_path="$1"
+
+        if [ ! -f "$file_path" ]; then
+            echo "No such file: $file_path"
+            exit 1
+        fi
+
+        file_type=`file -b --mime-type "$file_path"`
+
+        if [[ $file_type == image/* ]]; then
+            feh --no-fehbg --bg-fill $file_path
+            exit 0
+        fi
+
+        echo "Not wallpaper material: $file_path"
+        exit 1
+    fi
 fi
 
 #feh --no-fehbg --bg-scale '/home/coco/Pictures/Wallpapers/kame-house.jpg' 
