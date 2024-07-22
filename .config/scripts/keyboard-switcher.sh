4ea7d0670e2910783a9e71e3e9fa2297c01da06c
Coko <91132775+Coko7@users.noreply.github.com>
2024-07-22 13:16:45 +0200

    scripts: add icon when switching layout in keyboard-switcher.sh

--- .config/scripts/keyboard-switcher.sh
+++ .config/scripts/keyboard-switcher.sh
@@ -4,9 +4,12 @@ current_layout=`setxkbmap -query | grep layout | awk '{print $2}'`
 
 if [ "$current_layout" == "us" ]; then
   next_layout="fr"
-  setxkbmap -layout fr
+  layout_desc="AZERTY FR"
+  icon_path="azerty_icon.png"
 elif [ "$current_layout" == "fr" ]; then
   next_layout="us"
+  layout_desc="QWERTY US"
+  icon_path="qwerty_icon.png"
 fi
 
-setxkbmap -layout $next_layout && notify-send "Keyboard" "$next_layout layout"
+setxkbmap -layout $next_layout && notify-send "Keyboard Layout" "$layout_desc" -i "$HOME/Pictures/System/$icon_path"
