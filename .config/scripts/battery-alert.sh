63237e4d2207181683c6a02c26b0d80f2e949fc0
Coko <91132775+Coko7@users.noreply.github.com>
2024-07-22 10:57:00 +0200

    systemd: add timer and scripts for notifying when battery low

---
+++ .config/scripts/battery-alert.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+acpi -b | grep -Poq 'Discharging'
+
+# If grep succeeds, that means laptop is not plugged in
+if [ $? -eq 0 ]; then
+    battery_level=`cat /sys/class/power_supply/BAT1/capacity`
+
+    if [ $battery_level -le 5 ]; then
+
+        notify-send -u critical "System" \
+            "Battery: $battery_level%\nYoooo!! PLUG ME NOOOOW!!!" \
+            -i $HOME/Pictures/Memes/y-u-no.jpg
+
+    elif [ $battery_level -le 15 ]; then
+
+        notify-send -u normal "System" \
+            "Battery: $battery_level%\nGetting tired now, plug me soon pls" \
+            -i $HOME/Pictures/System/battery_low.png
+    fi
+fi
