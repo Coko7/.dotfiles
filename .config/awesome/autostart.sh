55bb0eef1720a4050df2863171df51b84e3a4089
Coko <91132775+Coko7@users.noreply.github.com>
2024-07-29 09:07:35 +0200

    awesome: run more programs at system startup

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
 run() {
-  if ! pgrep -f "$1" ;
-  then
-    "$@"&
-  fi
+    if ! pgrep -f "$1" ;
+    then
+        "$@"&
+    fi
 }
-
-source $SCRIPTS/set-wallpaper.sh
-set-wallpaper
 
 #run "megasync"
 ##run "xscreensaver -no-splash"
 #run "/usr/bin/dropbox"
 #run "insync start"
+
+# System processes
+run "nm-applet"
+run "sxhkd"
 run "picom"
+
+# Misc apps
+run "flameshot"
+run "keepassxc"
+
+# Messaging apps
+run "webcord"
+run "signal-desktop"
+
+# Office apps
+run "joplin-desktop"
+
+source $SCRIPTS/set-wallpaper.sh
+set-wallpaper
+
 # run "$XDG_CONFIG_HOME/polybar/launch.sh"
 #run "/usr/bin/redshift"
 ##run "mpd"
