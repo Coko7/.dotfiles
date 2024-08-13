dedb873edbf8f3299ac2505eb37f7fcbb661629f
Coko <91132775+Coko7@users.noreply.github.com>
2024-08-13 07:48:35 +0200

    X11: simplify screen layout setups

--- .config/screenlayout/multi-monitor-geo.sh
+++ .config/screenlayout/multi-monitor-geo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 2560x1600 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-5 --off --output DP-6 --off --output DP-7 --off --output DP-8 --off
+xrandr --output eDP-1 --auto --pos 0x0 --output DP-4 --primary --auto --pos 2560x0
