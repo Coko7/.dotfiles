321851880d7aa9322f7b37dce19490798d39b073
Coko <91132775+Coko7@users.noreply.github.com>
2024-08-25 15:08:49 +0200

    X11: add new screen layout

---
+++ .config/screenlayout/multi-monitor-curry-cinema.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 2560x1600 --pos 3840x0 --output DP-4 --mode 3840x2160 --primary --pos 0x0
--- .config/scripts/x-mon-select.sh
+++ .config/scripts/x-mon-select.sh
@@ -5,12 +5,15 @@ if [ -n "$1" ]; then
     opts="-f $1"
 fi
 
-configs="curry,nomad,geo,geo-cinema"
+configs="curry,curry-cinema,nomad,geo,geo-cinema"
 pick=`echo $configs | tr ',' '\n' | fzf $opts | head -n 1`
 
 case $pick in
   curry)
       export X_MY_LOC="curry"
+    ;;
+  curry-cinema)
+      export X_MY_LOC="curry-cinema"
     ;;
   geo)
       export X_MY_LOC="geo"
