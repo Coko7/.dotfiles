bc828b85a09701bbdbc3aac2864226ff43fa15b5
Coko <91132775+Coko7@users.noreply.github.com>
2024-10-16 21:08:28 +0200

    scripts(set-wallpaper): fix auto update wallpaper script

--- .config/scripts/set-wallpaper-script.sh
+++ .config/scripts/set-wallpaper-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
+source /home/coco/.config/scripts/cmd-utils.sh
 source /home/coco/.config/scripts/set-wallpaper.sh
 
-# Comment following line to disable dynamic wallpaper
 set-wallpaper
