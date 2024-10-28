eb4611265aed49618a52a8f7e6693e542f3266dd
Coko <91132775+Coko7@users.noreply.github.com>
2024-10-28 20:53:03 +0100

    script(play-pause-media): improve visuals + use wofi for hyrpland

--- .config/scripts/play-pause-media.sh
+++ .config/scripts/play-pause-media.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+function get_metadata_summary() {
+    local player=$1
+    local output=`playerctl -p $player metadata`
+
+    local title=`echo "$output" | grep -oP '.*xesam:title\s*\K.*'`
+    local album=`echo "$output" | grep -oP '.*xesam:album\s*\K.*'`
+    local artist=`echo "$output" | grep -oP '.*xesam:artist\s*\K.*'`
+
+    local res="$title"
+    if [[ -n "$album" ]]; then
+        res="$res, $album"
+    fi
+
+    if [[ -n "$artist" ]]; then
+        res="$res ($artist)"
+    fi
+
+    echo $res
+}
+
 players=`playerctl -l`
 player_count=`echo "$players" | wc -l`
 
@@ -8,9 +28,41 @@ if [ "$player_count" -eq 0 ]; then
 fi
 
 if [ "$player_count" -eq 1 ]; then
-    target=$players
-else
-    target=`echo "$players" | rofi -dmenu -p "Play/Pause:"`
+    playerctl -p $players play-pause
+    exit 0
+fi
+
+declare -A player_names
+counter=0
+for player in $players; do
+    player_names["$counter"]="$player"
+    ((counter++))
+done
+
+input=""
+for id in "${!player_names[@]}"; do
+    player_name="${player_names[$id]}"
+    metadata=`get_metadata_summary $player_name`
+    input="$input$metadata\n"
+done
+
+input=${input::-2}
+
+# target=`echo "$players" | rofi -dmenu -p "Play/Pause:"`
+pick=`echo -e "$input" | wofi --dmenu --prompt "Play/Pause:"`
+
+target=""
+for id in "${!player_names[@]}"; do
+    player_name="${player_names[$id]}"
+    metadata=`get_metadata_summary $player_name`
+    if [ "$metadata" = "$pick" ]; then
+        target="$player_name"
+        break
+    fi
+done
+
+if [ -z "$target" ]; then
+    exit 1
 fi
 
 playerctl -p $target play-pause
