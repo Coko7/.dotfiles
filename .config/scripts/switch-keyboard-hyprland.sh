7ebba37db7e1dc530c42b14580551445c4bd8549
Coko <91132775+Coko7@users.noreply.github.com>
2024-10-28 18:46:38 +0100

    scripts(switch-keyboard): rename script + provide version when using hyprland

---
+++ .config/scripts/switch-keyboard-hyprland.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# ================================ WARNING ==================================== #
+#                                                                               #
+# This script only works with Hyprland compositor / Window manager for Wayland. #
+#                                                                               #
+# ================================ WARNING ==================================== #
+
+function get_main_keyboard() {
+    hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .name'
+}
+
+KEYBOARD=`get_main_keyboard`
+
+function set_fr_azerty() {
+    # ID 1 for FR (see hyprland.conf)
+    hyprctl switchxkblayout $KEYBOARD 1 \
+        && notify-send "Keyboard Layout" "AZERTY Baguette" -i "$HOME/Pictures/System/azerty_icon2.png"
+}
+
+function set_us_qwerty() {
+    # ID 0 for EN (see hyprland.conf)
+    hyprctl switchxkblayout $KEYBOARD 0 \
+        && notify-send "Keyboard Layout" "QWERTY Burger" -i "$HOME/Pictures/System/kb_us_qwerty3.png"
+}
+
+function set_us_dvorak() {
+    # ID 2 for Dvorak (see hyprland.conf)
+    hyprctl switchxkblayout $KEYBOARD 2 \
+        && notify-send "Keyboard Layout" "QWERTY Dvorak" -i "$HOME/Pictures/System/kb_us_dvorak.png"
+}
+
+
+function get_current_layout() {
+    hyprctl devices -j | jq -r ".keyboards[] | \
+        select(.name == \"$KEYBOARD\") | .active_keymap"
+        # | cut -c1-2 | tr 'a-z' 'A-Z'
+}
+
+if [ -n "$1" ]; then
+    if [ "$1" == "dvorak" ]; then
+        set_us_dvorak
+        exit 0
+    fi
+fi
+
+current_layout=`get_current_layout`
+
+if [ "$current_layout" == "English (US)" ]; then
+    set_fr_azerty
+elif [ "$current_layout" == "English (Dvorak)" ]; then
+    set_us_qwerty
+else
+    set_us_qwerty
+fi
