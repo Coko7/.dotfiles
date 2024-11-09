bda624ec96ea97604720eef3f4ec299bd3facc88
Coko <91132775+Coko7@users.noreply.github.com>
2024-11-09 22:39:33 +0100

    scripts(wofi-power-menu): refactor script

--- .config/scripts/wofi-power-menu.sh
+++ .config/scripts/wofi-power-menu.sh
@@ -1,42 +1,41 @@
 #!/usr/bin/env bash
 
+function confirm_prompt() {
+    prompt=$1 
+    choice=`echo -e "Yes\nNo" | wofi --dmenu --prompt $prompt -W 15% -H 20%`
+
+    if [ "$choice" != "Yes" ]; then
+        return 1
+    fi
+
+    return 0
+}
+
 pick=`echo -e "󰐥 Shutdown\n󰜉 Reboot\n󰒲 Suspend\n󰍃 Logout\n󰌾 Lock" | wofi --dmenu --prompt "Action:" -i -W 20% -H 25%`
 formatted_pick=`echo $pick | awk '{print $2}'`
 
 case $formatted_pick in
 
     "Shutdown")
-        choice=`echo -e "Yes\nNo" | wofi --dmenu --prompt "UNPLUGGING ME???" -W 15% -H 20%`
-        case $choice in
-            Yes)
-                systemctl poweroff
-                ;;
-            *)
-                ;;
-        esac
+        confirm_prompt "UNPLUGGING ME???"
+        if [ $? -eq 0 ]; then
+            systemctl poweroff
+        fi
         ;;
     "Reboot")
-        choice=`echo -e "Yes\nNo" | wofi --dmenu --prompt "Time for a REBOOT?" -W 15% -H 20%`
-        case $choice in
-            Yes)
-                systemctl reboot
-                ;;
-            *)
-                ;;
-        esac
+        confirm_prompt "Time for a REBOOT?"
+        if [ $? -eq 0 ]; then
+            systemctl reboot
+        fi
         ;;
     "Suspend")
         systemctl suspend
         ;;
     "Logout")
-        choice=`echo -e "Yes\nNo" | wofi --dmenu --prompt "U sure about logging out?" -W 15% -H 20%`
-        case $choice in
-            Yes)
-                loginctl terminate-session
-                ;;
-            *)
-                ;;
-        esac
+        confirm_prompt "U sure about logging out?"
+        if [ $? -eq 0 ]; then
+            loginctl terminate-session
+        fi
         ;;
     "Lock")
         loginctl lock-session
