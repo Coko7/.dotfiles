7f0aee31ce134274c3ecf9639f6a4fc44f38ab2d
Coko <91132775+Coko7@users.noreply.github.com>
2025-01-13 07:48:01 +0100

    scripts(notify-media): replace grep with plctl format + temp dl img if not in fs

--- .config/scripts/notify-media.sh
+++ .config/scripts/notify-media.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+function get_meta() {
+    local player=$1
+    local key=$2
+    playerctl -p $player metadata --format "{{$key}}"
+}
+
 function show_notif() {
     local player=$1
-    local id=$2
+    local player_prefix=`echo $player | awk -F'.' '{print $1}'`
+    local notif_id=$2
 
-    local output=`playerctl -p $player metadata`
-
-    local title=`echo "$output" | grep -oP '.*xesam:title\s*\K.*'`
-    local album=`echo "$output" | grep -oP '.*xesam:album\s*\K.*'`
-    local artist=`echo "$output" | grep -oP '.*xesam:artist\s*\K.*'`
-    local art_url=`echo "$output" | grep -oP '.*mpris:artUrl\s*\K.*'`
+    local title=`get_meta "$player" "title"`
+    local album=`get_meta "$player" "album"`
+    local artist=`get_meta "$player" "artist"`
+    local art_url=`get_meta "$player" "mpris:artUrl"`
 
     if [[ -n "$album" ]]; then
         local body="$album\n$artist"
@@ -17,7 +22,21 @@ function show_notif() {
         local body="$artist"
     fi
 
-    notify-send "$title" "$body" -i "$art_url" -h "string:x-canonical-private-synchronous:cur-media-$id"
+    # image has to be downloaded
+    if [[ $art_url == http* ]]; then
+        temp_file=$(mktemp --suffix=.jpg)
+        wget -q -O "$temp_file" "$art_url"
+        img=$temp_file
+    else
+        img=$art_url
+    fi
+
+    # use fallback image if none available
+    if [ ! -f "$img" ]; then
+        img="$HOME/Pictures/System/music.jpg"
+    fi
+
+    notify-send "$title" "$body" -i "$img" -t 2000 -h "string:x-canonical-private-synchronous:cur-media-$notif_id"
 }
 
 id=0
