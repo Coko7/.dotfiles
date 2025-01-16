a33fa775b05eb972005b37d81a645d1764659f03
Coko <91132775+Coko7@users.noreply.github.com>
2025-01-16 07:48:11 +0100

    scripts(play-pause-media): fix stopped media appearing in menu + improve UI

--- .config/scripts/play-pause-media.sh
+++ .config/scripts/play-pause-media.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
+function get_meta() {
+    local player=$1
+    local key=$2
+    playerctl -p $player metadata --format "{{$key}}"
+}
+
 function get_metadata_summary() {
     local player=$1
     local output=`playerctl -p $player metadata`
 
-    local title=`echo "$output" | grep -oP '.*xesam:title\s*\K.*'`
-    local album=`echo "$output" | grep -oP '.*xesam:album\s*\K.*'`
-    local artist=`echo "$output" | grep -oP '.*xesam:artist\s*\K.*'`
+    local media_status=`playerctl -p $player status`
+    case $media_status in
+        Playing) label="";;
+        Paused) label="";;
+        Stopped) label="";;
+        *) ;;
+    esac
 
-    local res="$title"
-    if [[ -n "$album" ]]; then
-        res="$res, $album"
-    fi
+    local title=`get_meta "$player" "title"`
+    local artist=`get_meta "$player" "artist"`
 
+    local res="$label $player: $title"
     if [[ -n "$artist" ]]; then
         res="$res ($artist)"
     fi
@@ -42,6 +51,10 @@ done
 input=""
 for id in "${!player_names[@]}"; do
     player_name="${player_names[$id]}"
+    status=`playerctl -p $player_name status`
+    if [[ "$status" == "Stopped" ]]; then
+        continue
+    fi
     metadata=`get_metadata_summary $player_name`
     input="$input$metadata\n"
 done
@@ -49,7 +62,7 @@ done
 input=${input::-2}
 
 # target=`echo "$players" | rofi -dmenu -p "Play/Pause:"`
-pick=`echo -e "$input" | wofi --dmenu --prompt "Play/Pause:"`
+pick=`echo -e "$input" | rofi -dmenu -p " 󰐎 Play/Pause " -i -theme-str 'window {width: 800px;}'`
 
 target=""
 for id in "${!player_names[@]}"; do
