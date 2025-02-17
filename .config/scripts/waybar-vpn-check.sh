1951ca4cef90c6cb342bbc4c47b390d18a2ef2e7
Coko <91132775+Coko7@users.noreply.github.com>
2025-02-17 09:24:02 +0100

    waybar: add custom/vpn module

---
+++ .config/scripts/waybar-vpn-check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+VPN_STATE_FILE=$XDG_CONFIG_HOME/local/bin-sh/vpn_state.txt
+if [ -f $VPN_STATE_FILE ]; then
+    interface=`cat $VPN_STATE_FILE`
+    echo "{\"text\": \"$interface\", \"class\": \"active\" }"
+else
+    echo '{"text": "None", "class": "inactive" }'
+fi
