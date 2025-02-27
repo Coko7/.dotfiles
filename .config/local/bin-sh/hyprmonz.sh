130792f35858edefa88c68b4c523b164fe3a3016
Coko <91132775+Coko7@users.noreply.github.com>
2025-02-27 13:10:55 +0100

    bin-sh: add hyprmonz.sh to easily choose Hyprland monitor setup

---
+++ .config/local/bin-sh/hyprmonz.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+HYPR_DIR="$XDG_CONFIG_HOME/hypr"
+SETUPS_DIR="$HYPR_DIR/monitor_setups"
+MON_SYM_LNK="$HYPR_DIR/monitors.conf"
+
+pick=$(find "$SETUPS_DIR" -type f -printf "%f\n" \
+    | fzf -i --prompt="Monitor setup> " \
+    --preview="bat $SETUPS_DIR/{}")
+
+if [ -z "$pick" ]; then
+    exit 1
+fi
+
+if gum confirm "Are you sure you want to switch to $pick?" --default=false; then
+    target_mon_file="$SETUPS_DIR/$pick"
+
+    ln -vsf "$target_mon_file" "$MON_SYM_LNK"
+    hyprctl reload
+fi
