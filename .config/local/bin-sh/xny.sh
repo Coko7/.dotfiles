b8d858051c47b3acb190071f1a01c236b4b3de96
Coko <91132775+Coko7@users.noreply.github.com>
2025-03-13 06:51:34 +0100

    bin-sh(xny): fix shellcheck warnings + COBOL lang in json

--- .config/local/bin-sh/xny.sh
+++ .config/local/bin-sh/xny.sh
@@ -3,45 +3,46 @@
 # The xny_langs.json is a modified copy of: https://github.com/YesSeri/xny-cli/blob/417a25258ee3b8b9a46a05f43f0a355bf94b6815/data.json
 
 BASE_URL="https://learnxinyminutes.com"
-XNY_DATA=`jq '.' $XDG_CONFIG_HOME/local/bin-sh/xny_langs.json`
+XNY_DATA=$(jq '.' "$XDG_CONFIG_HOME/local/bin-sh/xny_langs.json")
 
 function display_lang_docs() {
-    local LANG_CODE=$1
-    local LANG_URL=$2
+    local lang_code=$1
+    local lang_url=$2
 
-    FULL_URL="$BASE_URL$LANG_URL"
+    full_url="$BASE_URL$lang_url"
 
-    local BAT_LANG_CODE=`bat --list-languages | grep -i "^$LANG_CODE:" \
-        | cut -d':' -f2 | cut -d',' -f1`
+    local bat_lang_code
+    bat_lang_code=$(bat --list-languages | grep -i "^$lang_code:" \
+        | cut -d':' -f2 | cut -d',' -f1)
 
-    if [ -n "$BAT_LANG_CODE" ]; then
-        curl -s $FULL_URL | bat -l $BAT_LANG_CODE -n
+    if [ -n "$bat_lang_code" ]; then
+        curl -s "$full_url" | bat -l "$bat_lang_code" -n
     else
-        curl -s $FULL_URL | bat -n
+        curl -s "$full_url" | bat -n
     fi
 }
 
 function find_lang() {
-    local INPUT=$1
-    echo $XNY_DATA | jq --arg lang $INPUT -r '.[] | select(.name == $lang) | .source_code_link'
+    local input=$1
+    echo "$XNY_DATA" | jq --arg lang "$input" -r '.[] | select(.name == $lang) | .source_code_link'
 }
 
 # If arg is provided, try to find the language
 if [ -n "$1" ]; then
-    INPUT=$1
-    XNY_LINK=`find_lang $INPUT`
-    if [ -n "$XNY_LINK" ]; then
-        display_lang_docs $INPUT $XNY_LINK
+    input=$1
+    xny_link=$(find_lang "$input")
+    if [ -n "$xny_link" ]; then
+        display_lang_docs "$input" "$xny_link"
     else
-        echo "xny: no such lang '$INPUT'" >&2
+        echo "xny: no such lang '$input'" >&2
         exit 1
     fi
     exit 0
 fi
 
 # If no arg provided, go into interactive mode
-XNY_LANG=`echo $XNY_DATA | jq -r '.[].name' | gum filter --prompt="Choose> "`
-if [ -n "$XNY_LANG" ]; then
-    XNY_LINK=`find_lang $XNY_LANG`
-    display_lang_docs $XNY_LANG $XNY_LINK
+xny_lang=$(echo "$XNY_DATA" | jq -r '.[].name' | gum filter --prompt="Choose> ")
+if [ -n "$xny_lang" ]; then
+    xny_link=$(find_lang "$xny_lang")
+    display_lang_docs "$xny_lang" "$xny_link"
 fi
