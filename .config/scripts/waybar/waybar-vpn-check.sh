d65c76e62cee88577db493cbb78c1d9711acfad1
Coko <91132775+Coko7@users.noreply.github.com>
2025-03-15 10:51:28 +0100

    scripts(waybar): fix vpn-check module

--- .config/scripts/waybar/waybar-vpn-check.sh
+++ .config/scripts/waybar/waybar-vpn-check.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-VPN_STATE_FILE=$XDG_CONFIG_HOME/local/bin-sh/vpn_state.txt
-if [ -f $VPN_STATE_FILE ]; then
-    interface=`cat $VPN_STATE_FILE`
+VPN_STATE_FILE=$XDG_CONFIG_HOME/local/bin-sh/global/data/vpn_state.txt
+if [ -f "$VPN_STATE_FILE" ]; then
+    interface=$(cat "$VPN_STATE_FILE")
     echo "{\"text\": \"$interface\", \"class\": \"active\" }"
 else
     echo '{"text": "None", "class": "inactive" }'
