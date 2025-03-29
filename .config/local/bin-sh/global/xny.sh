8d570c018182c6e2ed4e010e70d24bf513a4192c
Coko <91132775+Coko7@users.noreply.github.com>
2025-03-29 10:43:59 +0100

    bin-sh(global): edit xny.sh to cache docs and browse offline

--- .config/local/bin-sh/global/xny.sh
+++ .config/local/bin-sh/global/xny.sh
@@ -2,6 +2,7 @@
 
 # The xny_langs.json is a modified copy of: https://github.com/YesSeri/xny-cli/blob/417a25258ee3b8b9a46a05f43f0a355bf94b6815/data.json
 
+CACHE_DIR=/var/tmp/xny
 BASE_URL="https://learnxinyminutes.com"
 XNY_DATA=$(jq '.' "$XDG_CONFIG_HOME/local/bin-sh/global/data/xny_langs.json")
 
@@ -12,13 +13,26 @@ function display_lang_docs() {
     full_url="$BASE_URL$lang_url"
 
     local bat_lang_code
-    bat_lang_code=$(bat --list-languages | grep -i "^$lang_code:" \
+    bat_lang_code=$(bat --list-languages \
+        | grep -i "^$lang_code:" \
         | cut -d':' -f2 | cut -d',' -f1)
 
+    if [ ! -d "$CACHE_DIR" ]; then
+        mkdir -p $CACHE_DIR
+    fi
+
+    local lang_cache_file="$CACHE_DIR/$lang_code"
+    if [ ! -f "$lang_cache_file" ]; then
+        curl -s "$full_url" > "$lang_cache_file" || rm "$lang_cache_file"
+        if [ ! -f "$lang_cache_file" ]; then
+            echo "xny: failed to fetch xny for: $lang_code" | less
+        fi
+    fi
+
     if [ -n "$bat_lang_code" ]; then
-        curl -s "$full_url" | bat -l "$bat_lang_code" -n
+        bat -l "$bat_lang_code" -n "$lang_cache_file"
     else
-        curl -s "$full_url" | bat -n
+        bat -n "$lang_cache_file"
     fi
 }
 
@@ -34,7 +48,7 @@ if [ -n "$1" ]; then
     if [ -n "$xny_link" ]; then
         display_lang_docs "$input" "$xny_link"
     else
-        echo "xny: no such lang '$input'" >&2
+        echo "xny: no such lang: $input" >&2
         exit 1
     fi
     exit 0
