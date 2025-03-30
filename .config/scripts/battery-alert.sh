9f9ab8757b34a6273aa8cbff39284db80d202cb9
Coko <91132775+Coko7@users.noreply.github.com>
2025-03-30 21:04:53 +0200

    scripts: fix battery-alert by following shellcheck recos

--- .config/scripts/battery-alert.sh
+++ .config/scripts/battery-alert.sh
@@ -4,20 +4,20 @@ acpi -b | grep -Poq 'Discharging'
 
 # If grep succeeds, that means laptop is not plugged in
 if [ $? -eq 0 ]; then
-    battery_level=`cat /sys/class/power_supply/BAT1/capacity`
+    battery_level=$(cat /sys/class/power_supply/BAT1/capacity)
 
-    if [ $battery_level -le 15 ]; then
+    if [ "$battery_level" -le 15 ]; then
 
         notify-send -u critical "System" \
             "Battery: $battery_level%\nYoooo!! PLUG ME NOOOOW!!!" \
-            -i $HOME/Pictures/Memes/y-u-no.jpg \
+            -i "$HOME/Pictures/Memes/y-u-no.jpg" \
             -h string:x-canonical-private-synchronous:bat-alert
 
-    elif [ $battery_level -le 30 ]; then
+    elif [ "$battery_level" -le 30 ]; then
 
         notify-send -u normal "System" \
             "Battery: $battery_level%\nGetting tired now, plug me soon pls" \
-            -i $HOME/Pictures/System/battery_low.png \
+            -i "$HOME/Pictures/System/battery_low.png" \
             -h string:x-canonical-private-synchronous:bat-alert
     fi
 fi
