4a69f80e5e58f00e0e1d538b09647b915b74360d
Coko <91132775+Coko7@users.noreply.github.com>
2025-04-13 14:02:08 +0200

    bin-sh(global): add smb-mount.sh to replace old mount-nas-shares.sh + add bash-colors.sh in scripts

---
+++ .config/local/bin-sh/global/smb-mount.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+source "$SCRIPTS/bash-colors.sh"
+
+echo "SMntB" | figlet | lolcat -S 42
+
+server_ip=$(gum input --prompt="Server IP> ") || exit 1
+
+if ! gum spin --title="Pinging $server_ip..." -- ping -c1 "$server_ip"; then
+    echo -e "💀 ${FG_RED}Failed to ping: $server_ip"
+    exit 1
+fi
+
+smb_username=$(gum input --prompt="Username> ") || exit 1
+smb_password=$(gum input --prompt="Password> " --password) || exit 1
+
+smb_shares_raw=$(gum spin --title="Connecting to smb://$server_ip..." \
+    -- smbclient -L "$server_ip" --user="$smb_username" --password="$smb_password")
+if [[ $? -ne 0 ]]; then
+    echo -e "💀 ${FG_RED}Failed to connect to: smb://$server_ip"
+    exit 1
+fi
+
+smb_shares=$(echo "$smb_shares_raw" \
+    | grep 'Disk' | cut -d' ' -f1 \
+    | awk '{$1=$1; print}' | sort)
+
+echo -e "✨ ${FG_GREEN}Connected to: smb://$server_ip!"
+share_pick=$(echo "$smb_shares" | gum choose --header="Select SMB share:") || exit 1
+smb_mount_path="/mnt/smbshare/$server_ip/$share_pick"
+
+if mountpoint -q "$smb_mount_path"; then
+    echo "⛰️ $share_pick is already mounted at: $smb_mount_path"
+    if gum confirm "Do you want to unmount it?"; then
+        sudo umount "$smb_mount_path"
+        echo -e "❎ ${FG_GREEN}'$share_pick' has been unmounted"
+    fi
+
+    exit 0
+fi
+
+smb_mount_path=$(gum input --prompt="⛰️ Mount point for $share_pick> " --value="$smb_mount_path") || exit 1
+if ! gum confirm "Mount $share_pick to: $smb_mount_path?"; then
+    exit 0
+fi
+
+unix_user=$(whoami)
+
+# uid is needed to correctly setup file permissions and allow the current user to edit the remote files
+gum spin --title="Mounting $share_pick to $smb_mount_path..." \
+    -- sudo mount --mkdir -t cifs "//$server_ip/$share_pick" "$smb_mount_path" \
+    -o "uid=$unix_user,username=$smb_username,password=$smb_password,vers=2.0"
+
+echo -e "✅ ${FG_GREEN}'$share_pick' has been mounted!"
---
+++ .config/scripts/bash-colors.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Source this into scripts to use colored output without having to type the ANSI escape codes every time.
+
+# Reset color
+export COL_RESET='\e[0m'
+
+# Regular colors
+export FG_BLACK='\e[0;30m'
+export FG_RED='\e[0;31m'
+export FG_GREEN='\e[0;32m'
+export FG_YELLOW='\e[0;33m'
+export FG_BLUE='\e[0;34m'
+export FG_PURPLE='\e[0;35m'
+export FG_CYAN='\e[0;36m'
+export FG_GRAY='\e[0;37m'
+
+# Bold colors
+export B_BLACK='\e[1;30m'
+export B_RED='\e[1;31m'
+export B_GREEN='\e[1;32m'
+export B_YELLOW='\e[1;33m'
+export B_BLUE='\e[1;34m'
+export B_PURPLE='\e[1;35m'
+export B_CYAN='\e[1;36m'
+export B_GRAY='\e[1;37m'
+
+# Italic colors
+export I_BLACK='\e[3;30m'
+export I_RED='\e[3;31m'
+export I_GREEN='\e[3;32m'
+export I_YELLOW='\e[3;33m'
+export I_BLUE='\e[3;34m'
+export I_PURPLE='\e[3;35m'
+export I_CYAN='\e[3;36m'
+export I_GRAY='\e[3;37m'
+
+# Background colors
+export BG_BLACK='\e[40m'
+export BG_RED='\e[41m'
+export BG_GREEN='\e[42m'
+export BG_YELLOW='\e[43m'
+export BG_BLUE='\e[44m'
+export BG_PURPLE='\e[45m'
+export BG_CYAN='\e[46m'
+export BG_GRAY='\e[47m'
