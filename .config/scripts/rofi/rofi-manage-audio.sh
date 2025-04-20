6111b75eda566730fee280e23f542ee17ad342f4
Coko <91132775+Coko7@users.noreply.github.com>
2025-04-20 08:42:59 +0200

    scripts(rofi): improve scripts by following shellcheck hints

--- .config/scripts/rofi/rofi-manage-audio.sh
+++ .config/scripts/rofi/rofi-manage-audio.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-DEFAULT_SINK=`pactl get-default-sink`
-sinks=`pactl --format=json list sinks`
+DEFAULT_SINK=$(pactl get-default-sink)
+sinks=$(pactl --format=json list sinks)
 
-pick=`echo $sinks | jq -r --arg def_sink "$DEFAULT_SINK" '
+pick=$(echo "$sinks" | jq -r --arg def_sink "$DEFAULT_SINK" '
   map(
     if .name == $def_sink then
       "  " + .description
     else
       "  " + .description
     end
-  ) | .[]' | rofi -dmenu -p " 󰓃 Audio source " -i -theme-str 'window { width: 1000px; height: 270px; }'`
-formatted_pick=`echo $pick | cut -d' ' -f2-`
+    ) | .[]' | rofi -dmenu -p " 󰓃 Audio source " -i -theme-str 'window { width: 1000px; height: 270px; }')
+formatted_pick=$(echo "$pick" | cut -d' ' -f2-)
 
 if [ -n "$formatted_pick" ]; then
-    sink=`echo $sinks | jq --arg desc "$formatted_pick" -r '.[] | select(.description == $desc) | .name'`
-    pactl set-default-sink $sink
+    sink=$(echo "$sinks" | jq --arg desc "$formatted_pick" -r '.[] | select(.description == $desc) | .name')
+    pactl set-default-sink "$sink"
 fi
--- .config/scripts/rofi/rofi-play-pause-media.sh
+++ .config/scripts/rofi/rofi-play-pause-media.sh
@@ -3,14 +3,15 @@
 function get_meta() {
     local player=$1
     local key=$2
-    playerctl -p $player metadata --format "{{$key}}"
+    playerctl -p "$player" metadata --format "{{$key}}"
 }
 
 function get_metadata_summary() {
     local player=$1
-    local output=`playerctl -p $player metadata`
 
-    local media_status=`playerctl -p $player status`
+    local media_status
+    media_status=$(playerctl -p "$player" status)
+
     case $media_status in
         Playing) label="";;
         Paused) label="";;
@@ -18,26 +19,27 @@ function get_metadata_summary() {
         *) ;;
     esac
 
-    local title=`get_meta "$player" "title"`
-    local artist=`get_meta "$player" "artist"`
+    local title, artist
+    title=$(get_meta "$player" "title")
+    artist=$(get_meta "$player" "artist")
 
     local res="$label $player: $title"
     if [[ -n "$artist" ]]; then
         res="$res ($artist)"
     fi
 
-    echo $res
+    echo "$res"
 }
 
-players=`playerctl -l`
-player_count=`echo "$players" | wc -l`
+players=$(playerctl -l)
+player_count=$(echo "$players" | wc -l)
 
 if [ "$player_count" -eq 0 ]; then
     exit 0
 fi
 
 if [ "$player_count" -eq 1 ]; then
-    playerctl -p $players play-pause && bash $SCRIPTS/notify-media.sh
+    playerctl -p "$players" play-pause && bash "$SCRIPTS/notify-media.sh"
     exit 0
 fi
 
@@ -51,23 +53,23 @@ done
 input=""
 for id in "${!player_names[@]}"; do
     player_name="${player_names[$id]}"
-    status=`playerctl -p $player_name status`
+    status=$(playerctl -p "$player_name" status)
     if [[ "$status" == "Stopped" ]]; then
         continue
     fi
-    metadata=`get_metadata_summary $player_name`
+    metadata=$(get_metadata_summary "$player_name")
     input="$input$metadata\n"
 done
 
 input=${input::-2}
 
-# target=`echo "$players" | rofi -dmenu -p "Play/Pause:"`
-pick=`echo -e "$input" | rofi -dmenu -p " 󰐎 Play/Pause " -i -theme-str 'window {width: 800px;}'`
+# target=$(echo "$players" | rofi -dmenu -p "Play/Pause:")
+pick=$(echo -e "$input" | rofi -dmenu -p " 󰐎 Play/Pause " -i -theme-str 'window {width: 800px;}')
 
 target=""
 for id in "${!player_names[@]}"; do
     player_name="${player_names[$id]}"
-    metadata=`get_metadata_summary $player_name`
+    metadata=$(get_metadata_summary "$player_name")
     if [ "$metadata" = "$pick" ]; then
         target="$player_name"
         break
@@ -78,4 +80,4 @@ if [ -z "$target" ]; then
     exit 1
 fi
 
-playerctl -p $target play-pause && bash $SCRIPTS/notify-media.sh $target
+playerctl -p "$target" play-pause && bash "$SCRIPTS/notify-media.sh" "$target"
