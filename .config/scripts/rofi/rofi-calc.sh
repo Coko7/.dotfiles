b96bcc304dadea6df7556318c9a02c0b2b754b6c
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-05 16:05:38 +0200

    scripts(rofi/calc): set LC_MONETARY to EUR

--- .config/scripts/rofi/rofi-calc.sh
+++ .config/scripts/rofi/rofi-calc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rofi -show calc -modi calc -display-calc " 󰃬 Calculator " -no-show-match -no-sort 
+LC_MONETARY=de_DE.UTF-8 rofi -show calc -modi calc -display-calc " 󰃬 Calculator " -no-show-match -no-sort 
