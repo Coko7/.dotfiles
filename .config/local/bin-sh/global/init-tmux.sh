f6523cab91f5e19539189dadf773efd06730ba3d
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-07 10:44:04 +0200

    bin-sh(global): improve init-tmux.sh

--- .config/local/bin-sh/global/init-tmux.sh
+++ .config/local/bin-sh/global/init-tmux.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
-select_cmd="fzf"
+function activate_tmux_env() {
+    case $1 in
+        "Personal") bash "$SCRIPTS/tmux/init-perso-env.sh" ;;
+        "Work")     bash "$SCRIPTS/tmux/init-work-env.sh" ;;
+        "Demo")     bash "$SCRIPTS/tmux/init-demo-env.sh" ;;
+        *) exit 1 ;;
+    esac
+}
+
+select_cmd="fzf --multi"
 if [ -n "$1" ]; then
     select_cmd="fzf -f $1"
 fi
 
-spaces="Personal,Work"
-pick=$(echo $spaces | tr ',' '\n' | $select_cmd --prompt="Pick env > ")
+spaces="Personal\nWork\nDemo"
+pick=$(echo -e "$spaces" | $select_cmd --prompt="Pick env > " --preview="echo {} | figlet")
 
-case $pick in
-    "Personal") bash "$SCRIPTS/tmux/init-perso-env.sh" ;;
-    "Work") bash "$SCRIPTS/tmux/init-work-env.sh" ;;
-    *) return 1 ;;
-esac
+for env_name in $pick; do
+    echo "activating: $env_name"
+    activate_tmux_env "$env_name"
+done
+
+if tmux has-session 2>/dev/null; then
+    if gum confirm "Attach to tmux?"; then
+        tmux attach
+    fi
+fi
