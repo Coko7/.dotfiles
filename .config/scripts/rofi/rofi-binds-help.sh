2f14e363fa27356182ffbd0c6a9cfe3b08bd1ffe
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-14 08:50:39 +0200

    scripts(rofi/binds-help): improve visuals (add key symbols)

--- .config/scripts/rofi/rofi-binds-help.sh
+++ .config/scripts/rofi/rofi-binds-help.sh
@@ -1,37 +1,68 @@
 #!/usr/bin/env bash
 
 function get_modmask_label() {
+    local K_SUPER=""
+    local K_CTRL="󰘴"
+    local K_ALT="󰘵"
+    local K_SHIFT="󰘶"
     local modmask="$1"
     case "$modmask" in
-        0) echo "" ;;
-        1) echo "⇧" ;;
-        8) echo "⌥" ;;
-        12) echo "⌥⌃" ;;
-        64) echo "⌘" ;;
-        65) echo "⌘⇧" ;;
-        68) echo "⌘⌃" ;;
-        72) echo "⌘⌥" ;;
-        73) echo "⌘⌥⇧" ;;
+        0)  echo "" ;;
+        1)  echo "$K_SHIFT" ;;
+        8)  echo "$K_ALT" ;;
+        12) echo "${K_ALT}${K_CTRL}" ;;
+        64) echo "$K_SUPER" ;;
+        65) echo "${K_SUPER} ${K_SHIFT}" ;;
+        68) echo "${K_SUPER} ${K_CTRL}" ;;
+        72) echo "${K_SUPER} ${K_ALT}" ;;
+        73) echo "${K_SUPER} ${K_ALT}${K_SHIFT}" ;;
         *)
             echo "unsupported modmask: $modmask" >&2
-            exit 1
-            ;;
+            exit 1 ;;
+    esac
+}
+
+function get_key_label() {
+    case "$1" in
+        "space")                    echo "󱁐" ;;
+        "return")                   echo "󰌑" ;;
+        "comma")                    echo "," ;;
+        "period")                   echo "." ;;
+        "PRINT")                    echo "Prt" ;;
+        "mouse_down")               echo "󱕐" ;;
+        "mouse_up")                 echo "󱕑" ;;
+        "mouse:272")                echo "󰍽 (R)" ;;
+        "mouse:273")                echo "󰍽 (L)" ;;
+        "XF86AudioLowerVolume")     echo "󰝞" ;;
+        "XF86AudioMicMute")         echo "󰍭" ;;
+        "XF86AudioMute")            echo "󰝟" ;;
+        "XF86AudioNext")            echo "󰒭" ;;
+        "XF86AudioPause")           echo "󰏤" ;;
+        "XF86AudioPlay")            echo "󰐊" ;;
+        "XF86AudioPrev")            echo "󰒮" ;;
+        "XF86AudioRaiseVolume")     echo "󰝝" ;;
+        "XF86MonBrightnessDown")    echo "󱩎" ;;
+        "XF86MonBrightnessUp")      echo "󱩕" ;;
+        *)                          echo "$1" | tr '[:upper:]' '[:lower:]' ;;
     esac
 }
 
 function pretty_print_bind() {
-    local res modmask modmask_label key desc
+    local res modmask modmask_label key key_label desc
     res=$(echo "$1" | jq -r '"\(.modmask)@\(.key)@\(.description)"')
     modmask=$(echo "$res" | cut -d'@' -f1)
     modmask_label=$(get_modmask_label "$modmask")
     key=$(echo "$res" | cut -d'@' -f2)
+    key_label=$(get_key_label "$key")
+
     desc=$(echo "$res" | cut -d'@' -f3)
 
+    local prefix="[ "
     if [ -n "$modmask_label" ]; then
-        echo -n "$modmask_label"
+        prefix="[ $modmask_label "
     fi
 
-    echo "$key > $desc"
+    echo "${prefix}$key_label ] $desc"
 }
 
 function display_binds() {
@@ -40,4 +71,4 @@ function display_binds() {
     done
 }
 
-display_binds | rofi -dmenu -p "Search binds" -i -theme-str 'window {width: 1000px; height: 500px;}'
+display_binds | rofi -dmenu -p " 󱎸 Search bindings " -i -theme-str 'window {width: 1000px; height: 500px;}'
