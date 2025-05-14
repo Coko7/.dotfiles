58c044f7aeb8c8db23ac7755dd7f579d65ad15bd
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-14 08:51:16 +0200

    scripts(ocr-screen): tweak notify-send messages

--- .config/scripts/ocr-screen.sh
+++ .config/scripts/ocr-screen.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Set temporary file paths
+# Temporary file paths
 IMG_PATH="/tmp/ocr_screenshot_$$.png"
 TESSE_TXT_PATH="/tmp/ocr_text_$$"
 TXT_PATH="/tmp/ocr_text_$$.txt"
 
 if ! grim -g "$(slurp)" "$IMG_PATH"; then
-    notify-send -u low "Omega <CR>" "It was cancelled :(" \
+    notify-send -u low "Oh Captain, Read!" "It was cancelled 💀" \
         -i "$HOME/Pictures/System/ocr.jpg" -t 2000 \
         -h "string:x-canonical-private-synchronous:ocr-notif" --transient
     exit 0
@@ -14,13 +14,18 @@ fi
 
 # Preprocess image for better OCR
 # magick "$IMG_PATH" -resize 400% "$IMG_PATH"
+
 if ! tesseract "$IMG_PATH" "$TESSE_TXT_PATH" &> /dev/null; then
     echo "tesseract failed to process: $IMG_PATH"
     exit 1
 fi
 
-# Optional: Copy result to clipboard
-# cat "${TXT_PATH}.txt" | wl-copy
+notify-send -u low "Oh Captain, Read!" "Got some text🚀" \
+    -i "$HOME/Pictures/System/ocr.jpg" -t 2000 \
+    -h "string:x-canonical-private-synchronous:ocr-notif" --transient
+
+# Copy result to clipboard
+# cat "$TXT_PATH" | wl-copy
 
 xdg-open "$TXT_PATH"
 sleep 0.5s
