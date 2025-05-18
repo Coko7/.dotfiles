d4ec85097d9075b4f92a5fe1677b2f03f74e6102
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-18 18:40:01 +0200

    scripts(rofi/kanumi): update script to match new version of kanumi

--- .config/scripts/rofi/rofi-kanumi.sh
+++ .config/scripts/rofi/rofi-kanumi.sh
@@ -3,42 +3,47 @@
 function choice_prompt() {
     local prompt="  Kanumi "
     local choice
-    choice=$(echo -e "󰥨 Directory\n󰥷 Image\n󰒓 Configure" | rofi -dmenu -p "$prompt" -i -theme-str 'window {width: 250px; height: 230px;}')
+    choice=$(echo -e " Random\n󰥨 Directory\n󰥷 Image\n󰒓 Configure" | rofi -dmenu -p "$prompt" -i -theme-str 'window {width: 250px; height: 260px;}')
     echo "$choice" | cut -d' ' -f2- | tr '[:upper:]' '[:lower:]'
 }
 
+function pick_random() {
+    monitor_names=$(hyprctl monitors all -j | jq '.[].name' | tr -d '"')
+    for monitor in $monitor_names; do
+        img=$(kanumi filter | shuf | head -n 1)
+        swww img -o "$monitor" "$img" -t outer
+    done
+}
+
+function pick_dir() {
+    local all_dirs pick monitor_names
+
+    all_dirs=$(kanumi filter | rev | cut -d'/' -f2- | rev | sort -u | cut -d'/' -f6-)
+    pick=$(echo -e "$all_dirs" | rofi -dmenu -display-columns 1 -p " 󰥨 Directory " -theme-str 'window {width: 50%;}')
+    if [[ -z "$pick" ]]; then
+        exit 1
+    fi
+
+    local wp_dir="$WALLPAPERS_DIR/$pick"
+
+    monitor_names=$(hyprctl monitors all -j | jq '.[].name' | tr -d '"')
+    for monitor in $monitor_names; do
+        img=$(kanumi filter --directory "$wp_dir" | shuf | head -n 1)
+        swww img -o "$monitor" "$img" -t outer
+    done
+}
+
 function pick_image() {
-    pick=$(kanumi -t "$MODE" | cut -d'/' -f6- | rofi -dmenu -display-columns 1 -p " 󰥷 Image " -theme-str 'window {width: 50%;}')
+    pick=$(kanumi filter | cut -d'/' -f6- | rofi -dmenu -display-columns 1 -p " 󰥷 Image " -theme-str 'window {width: 50%;}')
     if [[ -z "$pick" ]]; then
         exit 1
     fi
 
     pick_full="$WALLPAPERS_DIR/$pick"
 
-    # get monitor names
     monitor_names=$(hyprctl monitors all -j | jq '.[].name' | tr -d '"')
-
-    # set wallpaper for each monitor
     for monitor in $monitor_names; do
         swww img -o "$monitor" "$pick_full" -t outer
-    done
-}
-
-function pick_dir() {
-    pick=$(kanumi -t "$MODE" | cut -d'/' -f6- | rofi -dmenu -display-columns 1 -p " 󰥨 Directory " -theme-str 'window {width: 50%;}')
-    if [[ -z "$pick" ]]; then
-        exit 1
-    fi
-
-    wp_dir="$WALLPAPERS_DIR/$pick"
-
-    monitor_names=$(hyprctl monitors all -j | jq '.[].name' | tr -d '"')
-
-    # set wallpaper for each monitor
-    for monitor in $monitor_names; do
-        # img=$(get_rand_img $wp_dir)
-        img=$(kanumi --type image "$wp_dir" | shuf | head -n 1)
-        swww img -o "$monitor" "$img" -t outer
     done
 }
 
@@ -46,10 +51,11 @@ function edit_config() {
     xdg-open "$XDG_CONFIG_HOME/kanumi/config.toml"
 }
 
-MODE=$(choice_prompt)
-case "$MODE" in
-    directory) pick_dir;;
-    image) pick_image;;
-    configure) edit_config;;
+mode=$(choice_prompt)
+case "$mode" in
+    random)     pick_random ;;
+    directory)  pick_dir ;;
+    image)      pick_image ;;
+    configure)  edit_config ;;
     *) exit 1;;
 esac
