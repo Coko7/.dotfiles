f80b3a41cd2d83d3dc4c46f03b8ab109a84dfb55
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-19 12:49:48 +0200

    bin-sh(global/lorem): update lorem.sh to support line/word/char limits + remove double <CR> in data file

--- .config/local/bin-sh/global/lorem.sh
+++ .config/local/bin-sh/global/lorem.sh
@@ -2,9 +2,30 @@
 
 LOREM_FILE="$XDG_CONFIG_HOME/local/bin-sh/global/data/lorem50.txt"
 
-paragraphs=10
-if [ -n "$1" ]; then
-    paragraphs=$(($1 * 2))
+if [ -z "$1" ]; then
+    head -n 5 "$LOREM_FILE"
+    exit 0
 fi
 
-head -n "$paragraphs" "$LOREM_FILE"
+limit="$1"
+
+# substr from 0..(limit.len() - 1) -> everything EXCEPT last char
+limit_count="${limit:0:${#limit}-1}"
+
+# substr from (limit.len() -1).. -> last char
+limit_type="${limit: -1}"
+
+case "$limit_type" in
+    # print first N lines/paragraphs
+    l|p) head -n "$limit_count" "$LOREM_FILE" ;;
+    # print first N words
+    w) 
+        tr -s '[:space:]' '\n' < "$LOREM_FILE" \
+            | head -n "$limit_count" \
+            | paste -sd' ' - ;;
+    # print first N chars
+    c) head -c "$limit_count" "$LOREM_FILE" ;;
+    *)
+        echo "lorem.sh: unknown limiter type '$limit_type'" >&2
+        exit 1 ;;
+esac
