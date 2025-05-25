eaa4d1efff9e112ae217d2bf081fe364b5f77ff0
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-26 00:07:54 +0200

    hyprland: enable Ergo-L (FR) layout + modify related keyboard scripts

--- .config/scripts/waybar/waybar-lang-select.sh
+++ .config/scripts/waybar/waybar-lang-select.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-CURRENT_LAYOUT=`hyprkeyb.sh get-active-layout`
+CURRENT_LAYOUT=$(hyprkeyb.sh get-active-layout)
 
 value=""
 case $CURRENT_LAYOUT in
-    "English (US)") value="US" ;;
-    "French") value="FR" ;;
+    "English (US)") value="QWE" ;;
+    "French") value="AZE" ;;
     "English (Dvorak)") value="DVO" ;;
+    "French (Ergo‑L)") value="Ergo-L" ;;
     *) value="$CURRENT_LAYOUT" ;;
 esac
 
