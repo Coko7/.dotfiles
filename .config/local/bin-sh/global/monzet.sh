46b50c8f892903da2fb6802f4fc828e636c7bb4e
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-26 00:06:11 +0200

    bin-sh(global/monzet): rename hyprmonz.sh to monzet.sh

--- .config/local/bin-sh/global/monzet.sh
+++ .config/local/bin-sh/global/monzet.sh
@@ -4,9 +4,10 @@ HYPR_DIR="$XDG_CONFIG_HOME/hypr"
 SETUPS_DIR="$HYPR_DIR/monitor_setups"
 MON_SYM_LNK="$HYPR_DIR/monitors.conf"
 
-pick=$(find "$SETUPS_DIR" -type f -printf "%f\n" \
-    | fzf -i --prompt="Monitor setup> " \
-    --preview="bat $SETUPS_DIR/{}")
+pick=$(find "$SETUPS_DIR" -type f -printf "%f\n" | fzf \
+    --border-label ' Interactive Monitor Setup ' --input-label ' Input ' \
+    --list-label ' Setups ' --preview-label ' File Preview ' \
+    --preview="bat $SETUPS_DIR/{} --language toml --color=always --style=plain")
 
 if [ -z "$pick" ]; then
     exit 1
@@ -16,7 +17,7 @@ if gum confirm "Are you sure you want to switch to $pick?" --default=true; then
     target_mon_file="$SETUPS_DIR/$pick"
     ln -vsf "$target_mon_file" "$MON_SYM_LNK"
 
-    if [ -n "${HYPRLAND_INSTANCE_SIGNATURE}" ]; then
+    if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
         hyprctl reload
     fi
 fi
