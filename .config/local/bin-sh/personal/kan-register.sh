144dbdadc9006eca9a4c48c4826211ef9a179c5e
Coko <91132775+Coko7@users.noreply.github.com>
2025-05-28 07:27:49 +0200

    bin-sh(personal): add kan-register.sh

---
+++ .config/local/bin-sh/personal/kan-register.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+metadatas_path=$(kanumi config show --json | jq -r '.meta_path')
+
+while true; do
+    pick=$(kanumi scan --json | jq -r '.new[]' | fzf \
+        --border-label ' Kanumi Interactive Register ' --input-label ' Input ' \
+        --list-label ' Unregistered Images ' --preview-label ' Image Preview ' \
+        --preview="$SCRIPTS/fzf-preview.sh {}" --height 80%)
+    if [ -z "$pick" ]; then
+        exit 1
+    fi
+
+    # kitten icat "$pick"
+    score=$(seq 0 10 | fzf \
+        --border-label ' Kanumi Register ' --input-label ' Input ' \
+        --list-label ' Boxa Scores ' --preview-label ' Image Preview ' \
+        --preview="$SCRIPTS/fzf-preview.sh $pick" --height 80%)
+    if [ -z "$score" ]; then
+        exit 1
+    fi
+
+    all_metas=$(cat "$metadatas_path")
+    if raw_json=$(kanumi meta gen "$pick"); then
+        json=$(echo "$raw_json" | jq)
+        new_json=$(echo "$json" | jq -c --argjson val "$score" '.scores += [{"name":"boxa", "value": $val}]')
+        all_metas=$(echo "$all_metas" | jq -c --argjson obj "$new_json" '. += [$obj]')
+        echo "$all_metas" > "$metadatas_path"
+    fi
+done
