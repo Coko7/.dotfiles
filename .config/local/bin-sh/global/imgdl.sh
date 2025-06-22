9ff00e914b356f0e9ba1d9dfae2658a75c083657
Coko <91132775+Coko7@users.noreply.github.com>
2025-06-22 05:55:01 +0200

    bin-sh(global): add ssh-muxor.sh + edit others

--- .config/local/bin-sh/global/imgdl.sh
+++ .config/local/bin-sh/global/imgdl.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
-url=$(gum input --prompt="URL> " --placeholder "Paste URL")
+echo "IMG-DL" | figlet | lolcat -S 11
+
+url=$(gum input --prompt="🌐 URL> " --placeholder "Paste URL")
 if [ -z "$url" ]; then
     exit 1
 fi
 
-echo "URL> $url"
+echo "🌐 URL> $url"
 
-filename=$(gum input --prompt="Save to> " --placeholder "example.png")
+default_filename=$(echo "$url" | rev | cut -d'/' -f1 | rev)
+filename=$(gum input --prompt="🖼️ Save to> " --value "$default_filename")
 if [ -z "$filename" ]; then
     exit 1
 fi
 
-echo "Save to> $filename"
+echo "🖼️ Save to> $filename"
 
 if [ -e "$filename" ]; then
     echo "imgdl: error: file already exists: $filename" >&2
     exit 1
 fi
 
-gum spin --title="Downloading $filename..." -- wget -O "$filename" "$url"
+gum spin --title="🚀 Downloading $filename..." -- wget -O "$filename" "$url"
