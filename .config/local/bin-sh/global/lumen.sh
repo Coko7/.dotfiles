1c976e6e8ca16dccb8e2b629293efce5fdf7a560
Coko <91132775+Coko7@users.noreply.github.com>
2025-07-30 12:02:37 +0200

    bin-sh(global/lumen): ensure arg is a num or exit 1

--- .config/local/bin-sh/global/lumen.sh
+++ .config/local/bin-sh/global/lumen.sh
@@ -6,6 +6,11 @@ if [ -z "$1" ]; then
 fi
 
 level=$1
+re='^[0-9]+$'
+if ! [[ $level =~ $re ]] ; then
+   echo "error: provide a number" >&2; exit 1
+fi
 
-gum spin --title="💡 Setting brightness to $level..." -- ddcutil setvcp 10 "$level" && \
-    brightnessctl set "${level}%"
+gum spin --title="💡 Setting brightness to $level..." \
+    -- ddcutil setvcp 10 "$level" \
+    && brightnessctl set "${level}%"
