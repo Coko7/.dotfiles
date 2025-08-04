9dfdb5519e3914b0aa1df7e80b79b314f72d723a
Coko <91132775+Coko7@users.noreply.github.com>
2025-08-04 15:53:22 +0200

    scripts(rofi/man-backlight): add more options

--- .config/scripts/rofi/rofi-man-backlight.sh
+++ .config/scripts/rofi/rofi-man-backlight.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-options="20%\n40%\n50%\n60%\n80%\n100%"
-pick=$(echo -e "$options" | rofi -dmenu -p " 󰛨 Backlight " -i -theme-str 'window { width: 250px; height: 220px; }')
+options="10%\n20%\n30%\n40%\n50%\n60%\n70%\n80%\n90%\n100%"
+pick=$(echo -e "$options" | rofi -dmenu -p " 󰛨 Backlight " -i -theme-str 'window { width: 250px; height: 300px; }')
 
 if [ -n "$pick" ]; then
     light=${pick::-1}
