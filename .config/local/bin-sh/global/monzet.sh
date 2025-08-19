29805f55a87bd5bed9910ab3143e129975541da5
Coko <91132775+Coko7@users.noreply.github.com>
2025-08-19 07:47:31 +0200

    bin-sh(global/monzet): improve script to also move workspaces to the correct monitor after changing layout

--- .config/local/bin-sh/global/monzet.sh
+++ .config/local/bin-sh/global/monzet.sh
@@ -19,5 +19,16 @@ if gum confirm "Are you sure you want to switch to $pick?" --default=true; then
 
     if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
         hyprctl reload
+
+        # attempts to move all existing workspaces to the new main monitor:
+        # - we assume the main monitor is not "eDP-1" (embedded Display Port for laptops)
+        # - we also assume the first monitor that is not "eDP-1" is the main monitor
+        main_monitor=$(hyprctl monitors -j | jq -r 'first(.[] | select(.name != "eDP-1") | .name)')
+        if [ -n "$main_monitor" ]; then
+            for workspace_id in $(hyprctl workspaces -j | jq '.[].id')
+            do
+                hyprctl dispatch moveworkspacetomonitor "$workspace_id" "$main_monitor"
+            done
+        fi
     fi
 fi
