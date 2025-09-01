51a538a490f7b1cb5b592eaeb8820182be6b4164
Coko <91132775+Coko7@users.noreply.github.com>
2025-09-01 16:32:14 +0200

    bin-sh(global/monzet): improve main monitor detection

--- .config/local/bin-sh/global/monzet.sh
+++ .config/local/bin-sh/global/monzet.sh
@@ -4,6 +4,15 @@ HYPR_DIR="$XDG_CONFIG_HOME/hypr"
 SETUPS_DIR="$HYPR_DIR/monitor_setups"
 MON_SYM_LNK="$HYPR_DIR/monitors.conf"
 
+function find_main_monitor() {
+    monitors=$(hyprctl monitors -j)
+    if [ "$(echo "$monitors" | jq 'length')" -gt 1 ]; then
+        echo "$monitors" | jq -r 'first(.[] | select(.name != "eDP-1") | .name)'
+    else
+        echo "$monitors" | jq -r 'first(.[].name)'
+    fi
+}
+
 pick=$(find "$SETUPS_DIR" -type f -printf "%f\n" | fzf \
     --border-label ' Interactive Monitor Setup ' --input-label ' Input ' \
     --list-label ' Setups ' --preview-label ' File Preview ' \
@@ -23,7 +32,7 @@ if gum confirm "Are you sure you want to switch to $pick?" --default=true; then
         # attempts to move all existing workspaces to the new main monitor:
         # - we assume the main monitor is not "eDP-1" (embedded Display Port for laptops)
         # - we also assume the first monitor that is not "eDP-1" is the main monitor
-        main_monitor=$(hyprctl monitors -j | jq -r 'first(.[] | select(.name != "eDP-1") | .name)')
+        main_monitor=$(find_main_monitor)
         if [ -n "$main_monitor" ]; then
             for workspace_id in $(hyprctl workspaces -j | jq '.[].id')
             do
