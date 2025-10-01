1f286301afd9fb14e873fa532aa67252f68730cb
Coko <91132775+Coko7@users.noreply.github.com>
2025-10-01 13:06:02 +0200

    bin-sh(global): improve multiple scripts

--- .config/local/bin-sh/global/lorem.sh
+++ .config/local/bin-sh/global/lorem.sh
@@ -2,6 +2,38 @@
 
 LOREM_FILE="$XDG_CONFIG_HOME/local/bin-sh/global/data/lorem50.txt"
 
+function show_help() {
+    cat <<EOF
+Usage: lorem.sh [N<type>] | [--help]
+
+Generate placeholder lorem ipsum text from \$LOREM_FILE.
+
+Arguments:
+  N<type>     Specify how much text to output:
+              Nl   Print first N lines
+              Np   Print first N paragraphs
+              Nw   Print first N words
+              Nc   Print first N characters
+
+  --help, -h  Show this help text
+
+Without arguments, the script will print the first 5 lines.
+
+Environment:
+  LOREM_FILE   Path to the lorem ipsum source file
+
+Examples:
+  lorem.sh 5l    # first 5 lines
+  lorem.sh 2p    # first 2 paragraphs
+  lorem.sh 25w   # first 25 words
+  lorem.sh 100c  # first 100 characters
+EOF
+}
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    show_help
+fi
+
 if [ -z "$1" ]; then
     head -n 5 "$LOREM_FILE"
     exit 0
@@ -26,6 +58,8 @@ case "$limit_type" in
     # print first N chars
     c) head -c "$limit_count" "$LOREM_FILE" ;;
     *)
-        echo "lorem.sh: unknown limiter type '$limit_type'" >&2
+        show_help
+        # echo "lorem.sh: unknown limiter type '$limit_type'" >&2
         exit 1 ;;
 esac
+
