cbec262cc528515938803bc3a652c56538a428eb
Coko <91132775+Coko7@users.noreply.github.com>
2025-10-09 08:32:18 +0200

    scripts(rofi/calc): set calc command and improve code readability

--- .config/scripts/rofi/rofi-calc.sh
+++ .config/scripts/rofi/rofi-calc.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-LC_MONETARY=de_DE.UTF-8 rofi -show calc -modi calc -display-calc " 󰃬 Calculator " -no-show-match -no-sort 
+LC_MONETARY=de_DE.UTF-8 rofi \
+    -show calc \
+    -modi calc \
+    -display-calc " 󰃬 Calculator " \
+    -no-show-match \
+    -no-sort \
+    -calc-command "echo -n '{result}' | wl-copy"
