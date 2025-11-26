d194667fc34cec79f4c250c1ec9b59a5e84a09e4
Coko <91132775+Coko7@users.noreply.github.com>
2025-11-26 17:48:55 +0100

    bin-sh(global/hook): fix script so that it works with filenames that have spaces in them

--- .config/local/bin-sh/global/hook.sh
+++ .config/local/bin-sh/global/hook.sh
@@ -5,15 +5,15 @@ DIRECTORY=$1
 [[ -z $DIRECTORY ]] && DIRECTORY="$HOME/Downloads"
 [[ ! -d $DIRECTORY ]] && echo "error: not a directory '$DIRECTORY'" && exit 1
 
-selected_files=$(find "$DIRECTORY" -type f -printf '%T@ %p\n' \
+mapfile -t selected_files < <(find "$DIRECTORY" -type f -printf '%T@ %p\n' \
     | sort -nr \
     | cut -d' ' -f2- \
     | fzf --delimiter '/' --with-nth 5.. --multi \
     --preview="bat --color=always --style=plain {}")
 
-[[ -z $selected_files ]] && exit 0
+[[ ${#selected_files[@]} -eq 0 ]] && exit 0
 
-for file in ${selected_files}; do
+for file in "${selected_files[@]}"; do
     [[ ! -f $file ]] && echo "error: no such file '$file'" && exit 1
     mv "$file" . && echo "🪝 moved '$file' to '$(pwd)'"
 done
