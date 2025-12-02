0a48ca8ce42d957ff9233c95109d54eb2a7d8b0a
Coko <91132775+Coko7@users.noreply.github.com>
2025-12-02 09:11:17 +0100

    bin-sh(global/pomodoro): can now specify work/rest time via cmd arg

--- .config/local/bin-sh/global/pomodoro.sh
+++ .config/local/bin-sh/global/pomodoro.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 SOUND_DIR="/usr/share/sounds/freedesktop/stereo"
-WORK_TIME="25m"
-BREAK_TIME="5m"
+DEFAULT_WORK_TIME="30m"
+DEFAULT_BREAK_TIME="5m"
 
 function work() {
-    timer $WORK_TIME -n "🍅 Pomodoro: Work time" --format 24h \
-        && notify-send -u low "🍅 Break Time!" "Your $WORK_TIME work session has just ended" \
+    [[ -z "$1" ]] && echo "error: expects work time to be passed as \$1" && exit 1
+    timer "$1" -n "🍅 Pomodoro: Work time" --format 24h \
+        && notify-send -u low "🍅 Break Time!" "Your $1 work session has just ended" \
         -i "$HOME/Pictures/System/tomato.png" \
         -h string:x-canonical-private-synchronous:pomodoro-mine --transient \
         && paplay "$SOUND_DIR/complete.oga"
 }
 
 function rest() {
-    timer $BREAK_TIME -n "🍅 Pomodoro: Break time" --format 24h \
+    [[ -z "$1" ]] && echo "error: expects rest time to be passed as \$1" && exit 1
+    timer "$1" -n "🍅 Pomodoro: Break time" --format 24h \
         && notify-send -u low "🍅 Break is over!" "Get back to work" \
         -i "$HOME/Pictures/System/tomato.png" \
         -h string:x-canonical-private-synchronous:pomodoro-mine --transient \
@@ -23,13 +25,19 @@ function rest() {
 function show_help() {
     echo -e "Usage: pomodoro.sh OPTION\n"
     echo "-h, --help        display this help text and exit"
-    echo "-w, --work        start $WORK_TIME work timer"
-    echo "-r, --rest        start $BREAK_TIME break timer"
+    echo "-w, --work        start $DEFAULT_WORK_TIME work timer"
+    echo "-r, --rest        start $DEFAULT_BREAK_TIME break timer"
 }
 
 case "$1" in
-    "-w"|"--work") work ;;
-    "-r"|"--rest") rest ;;
+    "-w"|"--work")
+        work_time=${2:-$DEFAULT_WORK_TIME}
+        work "$work_time"
+        ;;
+    "-r"|"--rest")
+        rest_time=${2:-$DEFAULT_REST_TIME}
+        rest "$rest_time"
+        ;;
     "-h"|"--help") show_help ;;
     *) show_help; exit 1 ;;
 esac
