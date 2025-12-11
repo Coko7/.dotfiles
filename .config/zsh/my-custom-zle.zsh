869f3777bfa78b7216d3a1d5d8e80ce424f12285
Coko <91132775+Coko7@users.noreply.github.com>
2025-12-11 17:01:52 +0100

    zsh(zle): update widgets and keybinds

--- .config/zsh/my-custom-zle.zsh
+++ .config/zsh/my-custom-zle.zsh
@@ -6,7 +6,7 @@ function zoxide_prompt_interactive() {
     zle reset-prompt
 }
 zle -N zoxide_prompt_interactive
-bindkey '^E' zoxide_prompt_interactive
+bindkey '^J' zoxide_prompt_interactive
 
 # Custom ZLE widget to launch yazi interactive with CTRL+N(avigate)
 function yazi_interactive() {
@@ -17,12 +17,12 @@ zle -N yazi_interactive
 bindkey '^N' yazi_interactive
 
 # Custom ZLE widget to launch neovim interactive with CTRL+T(ext Edit)
-function neovim_interactive() {
-    nvim
-    zle reset-prompt
-}
-zle -N neovim_interactive
-bindkey '^T' neovim_interactive
+# function neovim_interactive() {
+#     nvim
+#     zle reset-prompt
+# }
+# zle -N neovim_interactive
+# bindkey '^T' neovim_interactive
 
 # Custom ZLE widget to copy current line to clipboard
 function copy_line_to_clipboard() {
@@ -42,3 +42,35 @@ function _my_ollama_commit_msg_generator() {
 }
 zle -N _my_ollama_commit_msg_generator
 bindkey '^ga' _my_ollama_commit_msg_generator
+
+function _my_commit_messages_fzf() {
+    local msg
+    msg=$(git log --pretty=format:"%s" | fzf) || msg=""
+    BUFFER="${BUFFER}${msg}"
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N _my_commit_messages_fzf
+bindkey '^gc' _my_commit_messages_fzf
+
+# Disabled because I use ZVM 'vv' mode instead for now
+# Custom widget that uses nvim to edit cmd prompt
+# function edit_with_nvim() {
+#     # Use a unique temporary file
+#     local tmpfile=$(mktemp /tmp/zsh_edit.XXXXXX)
+#
+#     # Write current buffer to the file
+#     print -r -- "$BUFFER" > "$tmpfile"
+#
+#     # Open in Neovim
+#     nvim "+set filetype=sh" "$tmpfile"
+#
+#     # Restore edited content back into ZLE buffer
+#     BUFFER=$(<"$tmpfile")
+#     CURSOR=${#BUFFER}
+#
+#     # Clean up
+#     command rm "$tmpfile"
+# }
+# zle -N edit_with_nvim
+# bindkey '^E' edit_with_nvim
